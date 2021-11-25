bb20382ab96b1a725d2a52495a54a8ca79857469
Jnv821 <95052283+Jnv821@users.noreply.github.com>
2021-11-26 00:36:14 +0100

    Add files via upload

--- 108scriptsh.sh
+++ 108scriptsh.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ---------------------------------------------------------------------------------
 #                  INFORMACIÓN CLAVE DEL ARCHIVO SHELL SCRIPT
 # ---------------------------------------------------------------------------------
@@ -8,11 +9,9 @@
 #   ===============================================================================
 #   Descripción: 
 #  
-#   Este Script Bash automatizara la creación de ciertas tareas sin la posiblidiad
-#   de personalización. Desplegará un par de host virtuales y wordpress de forma 
-#   predefinida y no servirá en una instalación limpia de apache.
-#
-#   NOTA: Se puede hacer pero dispongo de poco tiempo para completar esta actividad.
+#   Este Script Bash automatizara la creación de ciertas tareas. 
+#   Desplegará un par de host virtuales y wordpress.
+#   
 #   ===============================================================================
 #   Valores Predefinidos:
 #       
@@ -41,19 +40,9 @@
 #                   Certificado SSL: hosting108.pem
 #                   Paginas de Error: Mensaje de Error sencillo
 #
-# =============================================================================
-#         IMPORTANTE LEER, DUDAS SOBRE LA REALIZACION DE LA ACTIVIDAD 
-#                                Y WORDPRESS.
-# =============================================================================
-#
-#   Tengo una confusión con el enunciado y no me queda muy claro.
-#   No estoy seguro de si tengo que añadir las paginas de los virtual host a Wordpress
-#   o no, por eso Instalaré wordpress de la misma forma que se hizo en la actividad
-#   107, exceptuando por aquellos pasos que se piden no automatizar.
 # ==============================================================================
 #                   ESTRUCTURA DE FICHEROS QUE SE CREARA
 # ==============================================================================
-#   Por ende el script generará algo parecido a esta estructura de ficheros:
 #   
 #   /var/www/
 #           |
@@ -102,35 +91,126 @@
 #                       COMIENZO DEL SCRIPT BASH
 # ===========================================================================
 
+# Estuve teniendo porblemas para que echo -e ...\n... hiciese un salto de linea.
+# por eso echo " " estara presente.
+echo "Bienvenido a la herramienta de creacion de VirtualHost e implantacion de Wordpress Automatica."
+echo "Los hots virtuales creados con esta herramienta utilizan el nombre"
+echo "El nombre se añadira a 'ubuntuserver.local' y en caso de ser https se añadira '-ssl'"
+echo
+echo "Primero necesitamos unos datos para los hosts virtuales:"
+echo 
+#------------WHILE DE CONFIRMACION DE ENTRADA DE DATOS.------------------------
+while true; do
+read -p "Nombre para los Hosts virtuales: " ServerName
+echo
+read -p "Confirma que el nombre es correcto (Y/N)" isnameok
+    case $isnameok in
+        [yYeEsS]) 
+                    echo "El nombre ha sido confirmado"
+                    break
+                    ;;
+        [nNoO])
+                    >&2
+                    ;;
+        *^[yYeEsSnN])
+                    echo "Introduzca un caracter valido" >&2
+                    echo
+    esac 
+done
+echo
+#------------WHILE DE CONFIRMACION DE CORREO ELECTRONICO------------------------
+echo "Es necesario que introduzca un correo electronico para la administracion del servidor"
+while true; do
+read -p "Email de Administración: " Email
+echo
+read -p "Confirma que el email es correcto (Y/N)" isemailok
+    case $isemailok in
+        [yYeEsS]) 
+                    echo "El nombre ha sido confirmado"
+                    break
+                    ;;
+        [nNoO])
+                    >&2
+                    ;;
+        *^[yYeEsSnN])
+                    echo "Introduzca un caracter valido" >&2
+                    echo
+    esac 
+done
+echo
+#------------WHILE DE CONFIRMACION DE LA LLAVE Y CERTIFICADO SSL------------------------
+echo "Es necesario elegir el nombre del archivo de la llave para el uso de HTTPS"
+while true; do
+read -p "Nombre para el archivo de la llave: " KeyName
+echo
+read -p "Confirma que el nombre es correcto (Y/N)" iskeynameok
+    case $iskeynameok in
+        [yYeEsS]) 
+                    echo "El nombre ha sido confirmado"
+                    break
+                    ;;
+        [nNoO])
+                    >&2
+                    ;;
+        *^[yYeEsSnN])
+                    echo "Introduzca un caracter valido" >&2
+                    echo
+    esac 
+done
+echo
+echo "Es necesario elegir el nombre del archivo de certificado de la llave para el uso de HTTPS"
+#-----------------------------------CERTIFICADO------------------------------------------------
+echo "Es necesario elegir el nombre del archivo deL certificado para el uso de HTTPS"
+while true; do
+read -p "Nombre para el archivo de la llave: " CertName
+echo
+read -p "Confirma que el nombre es correcto (Y/N)" iscertnameok
+    case $iscertnameok in
+        [yYeEsS]) 
+                    echo "El nombre ha sido confirmado"
+                    break
+                    ;;
+        [nNoO])
+                    >&2
+                    ;;
+        *^[yYeEsSnN])
+                    echo "Introduzca un caracter valido" >&2
+                    echo
+    esac 
+done
+echo
 
-echo Este Script generara 2 Virtual Hosts predefinidods e Instalara Wordpress.
-Es probable que necesite autentificarse como Super usuario en diferent ocasiones.
+echo "Se ha terminado de recopliar los datos, se pasara al proceso de creacion"
 
-# Genera el directorio para almacenar hosting 108
+#=========================== FIN DE LA RECOPILACION DE DATOS ===================================
 
-echo Generando directorio para guardar hosting108 y hosting108-ssl
+#========================== GENERANDO LOS DIRECTORIOS Y FICHEROS ===============================
 
-sudo mkdir /var/www/hosting108
-sudo mkdir /var/www/hosting108-ssl
+# Genera la estructura de ficheros basada en las variables.
 
-echo Se han generado los directorios correctamente.
+echo "Generando directorios para guardar hosting108, hosting108-ssl y todo su contenido"
 
-echo Generando el archivo de configuracion para hosting108
+sudo mkdir -p "/var/www/$ServerName"/ErrorDocs
+sudo mkdir -p "/var/www/$ServerName"-ssl/ErrorDocs
 
- Genera el archivo de Virtual Host con todo el contenido necesario y limpia la terminal.
-cat > /etc/apache2/sites-available/hosting108.conf << EOF
+echo "Se han generado los directorios correctamente."
+
+"echo Generando el archivo de configuracion para $ServerName"
+
+# Genera el archivo de Virtual Host con todo el contenido necesario y limpia la terminal.
+cat > /etc/apache2/sites-available/$ServerName.conf << EOF
 <VirtualHost *:80>
 
-    ServerName hosting108.ubuntuserver.local
-    ServerAlias www.hosting108.ubuntuserver.local
-    ServerAdmin a20joseRF@iessanclemente.net
-    DocumentRoot /var/www/hosting108
+    ServerName $ServerName.ubuntuserver.local
+    ServerAlias www.$ServerName.ubuntuserver.local
+    ServerAdmin $email
+    DocumentRoot /var/www/$ServerName
 
     # Apartado de Redirecciones
 
     <IfModule mod_rewrite.c>
         RewriteEngine On
-        RewriteRule ^(.*)$ https://hosting108.ubuntuserver.local$1 [L,R=302]
+        RewriteRule ^(.*)$ https://$ServerName.ubuntuserver.local$1 [L,R=302]
     </IfModule>
 
     # Apartado de los documentos de errores
@@ -144,4 +224,190 @@ cat > /etc/apache2/sites-available/hosting108.conf << EOF
 </VirtualHost>
 EOF
 
-echo Se ha generado el archivo de configuración para hosting108
+echo "Se ha generado el archivo de configuración para $ServerName"
+
+# Genera TODO el contenido para la estructura de ficheros de hosting 108.
+
+echo "Se generará el contenido para $ServerName Este proceso podria tardar un rato." 
+
+#============== GENERANDO CONTENIDO PARA EL PRIMER VIRTUAL HOST ==================
+
+#=========== DESCARGANDO IMAGEN DE FORMA OCULTA PARA UN PROCESO MAS FLUIDO =======
+
+wget -q https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Gnu-bash-logo.svg/1920px-Gnu-bash-logo.svg.png --output-file /var/www/$ServerName/GNUBASHLOGO.PNG
+
+#========== Generando Index.html ==================================================
+echo "Genrando index.html..."
+cat > /var/www/$ServerName/index.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Jose Rubicco Ferro">
+    <title>Shell Script Site</title>
+    <link rel="stylesheet" href="Main.css">
+</head>
+<body>
+    
+    <img src="GNUBASHLOGO.png" alt="BASH LOGO">
+    
+    <div class="div1">
+        <h1>La creación de los Virtual Host fue existosa.</h1>
+    </div>
+       
+    
+    <ul>
+        <li>
+            <p>Ficheros para http y https creados correctamente</p>
+        </li>
+        <li>
+            <p>Redirección http --> https</p>
+        </li>
+        <li>
+            <p>Para confirmar la parte relacionada con Wordpress acceda a <a href="https://ubuntuserver.local">ubuntuserver.local</a></p>
+        </li>
+    </ul>
+</body>
+</html>
+EOF
+#============================ GENERANDO ARCHIVO CSS ===================================================
+echo "Genrando Archivo CSS..."
+cat > /var/www/$ServerName/Main.css << EOF
+body{
+    background-color: rgb(235, 235, 235);
+}
+
+img{
+    width: 50%;
+    height: auto;
+    margin-left: 25%;
+    overflow: hidden;
+    position: relative;
+}
+h1{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    text-align: center;
+}
+.div1{
+    margin-left: 25%;
+    width:50%;
+    background-color: rgb(181, 181, 181);
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+}
+
+ul{
+    width: 50%;
+ margin-left: 25%;
+}
+
+li{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 24px;
+}
+
+a{
+    font-family:  'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: black;
+}
+a:visited{
+    color: gray
+}
+a:hover{
+    color: green;
+}
+a:active{
+    color: white
+}
+.errorp{
+        width: 50%;
+        margin-left: 25%;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 24px;
+}
+EOF
+#============================ GENERANDO ARCHIVOS DE ERROR =============================================
+
+#------------------------------------ERROR 403----------------------------------------------------------
+echo "Genrando Archivo de Error 403..."
+cat > /var/www/$ServerName/403.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Jose Rubicco Ferro">
+    <title>403</title>
+    <link rel="stylesheet" href="../Main.css">
+</head>
+<body>
+    
+    <img src="../GNUBASHLOGO.png" alt="BASH LOGO">
+    
+    <div class="div1">
+        <h1>Error 403: SIN AUTORIZACIÓN</h1>
+    </div>
+    
+    <p class="errorp">Porfavor contacte con el administrador para solucionar este problema y obtener permisos.</p>
+</body>
+</html>
+EOF
+
+#------------------------------------ERROR 404----------------------------------------------------------
+echo "Genrando Archivo de Error 404..."
+cat > /var/www/$ServerName/404.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Jose Rubicco Ferro">
+    <title>403</title>
+    <link rel="stylesheet" href="../Main.css">
+</head>
+<body>
+    
+    <img src="../GNUBASHLOGO.png" alt="BASH LOGO">
+    
+    <div class="div1">
+        <h1>Error 404: NO ENCONTRADO</h1>
+    </div>
+    
+    <p class="errorp">No se ha encontrado la página solicitada, revise el URL o contacte al administrador para solucionar este problema.</p>
+</body>
+</html>
+EOF
+
+#------------------------------------ERROR 500----------------------------------------------------------
+echo "Genrando Archivo de Error 500..."
+cat > /var/www/$ServerName/500.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Jose Rubicco Ferro">
+    <title>403</title>
+    <link rel="stylesheet" href="../Main.css">
+</head>
+<body>
+    
+    <img src="../GNUBASHLOGO.png" alt="BASH LOGO">
+    
+    <div class="div1">
+        <h1>Error 500: ERROR INTERNO DEL SERVIDOR</h1>
+    </div>
+    
+    <p class="errorp">Hay un error en la configuración del servidor porfavor contacte con el administrador.</p>
+</body>
+</html>
+EOF
+
+#===================================FIN DE LA GENERACION DE CONTENIDO PARA HTTP ============================
+
