06e5e11c89f1300007070a3967b09e8e30eab559
Jnv821 <95052283+Jnv821@users.noreply.github.com>
2021-11-26 01:45:23 +0100

    Add files via upload

--- 108scriptsh.sh
+++ 108scriptsh.sh
@@ -176,7 +176,7 @@ echo "Se han generado los directorios correctamente."
 echo "Generando el archivo de configuracion para $ServerName"
 
 # Genera el archivo de Virtual Host con todo el contenido necesario
-sudo cat > /etc/apache2/sites-available/$ServerName.conf << EOF
+sudo bash -c cat > /etc/apache2/sites-available/$ServerName.conf << EOF
 <VirtualHost *:80>
 
     ServerName $ServerName.ubuntuserver.local
@@ -216,7 +216,7 @@ sudo wget -q https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Gnu-bash-
 
 #========== Generando Index.html ==================================================
 echo "Genrando index.html..."
-sudo touch /var/www/$ServerName/index.html << EOF
+sudo bash -c cat /var/www/$ServerName/index.html << EOF
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -252,7 +252,7 @@ sudo touch /var/www/$ServerName/index.html << EOF
 EOF
 #============================ GENERANDO ARCHIVO CSS ===================================================
 echo "Genrando Archivo CSS..."
-sudo touch > /var/www/$ServerName/Main.css << EOF
+sudo bash -c cat > /var/www/$ServerName/Main.css << EOF
 body{
     background-color: rgb(235, 235, 235);
 }
@@ -311,7 +311,7 @@ EOF
 
 #------------------------------------ERROR 403----------------------------------------------------------
 echo "Genrando Archivo de Error 403..."
-sudo touch > /var/www/$ServerName/403.html << EOF
+sudo bash -c cat > /var/www/$ServerName/403.html << EOF
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -337,7 +337,7 @@ EOF
 
 #------------------------------------ERROR 404----------------------------------------------------------
 echo "Genrando Archivo de Error 404..."
-sudo touch > /var/www/$ServerName/404.html << EOF
+sudo bash -c cat > /var/www/$ServerName/404.html << EOF
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -363,7 +363,7 @@ EOF
 
 #------------------------------------ERROR 500----------------------------------------------------------
 echo "Generando Archivo de Error 500..."
-sudo touch > /var/www/$ServerName/500.html << EOF
+sudo bash -c cat > /var/www/$ServerName/500.html << EOF
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -392,7 +392,7 @@ EOF
 #=================================== INICIO DE LA GENERACIÓN DE HOST VIRTUAL PARA HTTPS =======================
 echo "Generando archivo de configuracion para $ServerName-ssl"
 
-sudo touch > "/etc/apache2/sites-available/$ServerName"-ssl.conf << EOF
+sudo bash -c cat > "/etc/apache2/sites-available/$ServerName"-ssl.conf << EOF
 <IfModule mod_ssl.c>
     <VirtualHost *:80>
 
