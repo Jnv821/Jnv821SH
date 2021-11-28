b3933a314b1167f308fa6f73dbf28a5a6e060de7
Jnv821 <95052283+Jnv821@users.noreply.github.com>
2021-11-28 22:17:22 +0100

    Add files via upload

--- delete-vhost-apache2.sh
+++ delete-vhost-apache2.sh
@@ -16,33 +16,32 @@
 #============SCRIPT PARA LA ELIMINACIÓN DE LOS ARCHIVOS Y VOLVER A BASE==============
 read -p "Nombre del servidor que será borrado " ServerName
 
-#----- Remueve todas las paginas web -----
+#----- Remueve todas las paginas web y archivo descargado de wordpress-----
 echo "Removiendo las carpetas de contenido de los host virtuales"
-sudo rm  /var/www/$ServerName/*
-sudo rm  /var/www/$ServeName-ssl/*
-sudo rm -d /var/www/$ServeName-ssl
-sudo rm -d /var/www/$ServeName
-#sudo rm -dR /var/www/html
-#sudo rm -dR /var/www/wordpress
+sudo rm -d -R /var/www/$ServerName/*
+sudo rm -d -R /var/www/$ServerName-ssl/*
+sudo rm -dR /var/www/html
+sudo rm /var/www/wordpres*
 
 #----- Remueve los archivos del certificado y la llave ------
 # Obtención de la llave y certificado
-KeyName=$ServeName.key
-CertName=$ServeName.pem
+KeyName=$ServerName.key
+CertName=$ServerName.pem
 
 echo "Removiendo archivos Archivos de llave y certificados"
-sudo rm /etc/ssl/private/$KeyName.key
-sudo rm /etc/ssl/certs/$CertName.pem
+sudo rm /etc/ssl/private/$KeyName
+sudo rm /etc/ssl/certs/$CertName
 
 #----- Remueve los archivos de host virtuales ---------
 
 echo "Removiendo archivos de Hosts Virtuales" 
 sudo rm /etc/apache2/sites-available/000-default.conf
-sudo rm /etc/apache2/sites-available/$ServeName.conf
-sudo rm /etc/apache2/sites-available/$ServeName-ssl.conf
+sudo rm /etc/apache2/sites-available/$ServerName.conf
+sudo rm /etc/apache2/sites-available/$ServerName-ssl.conf
 
 #========== RESTAURAR LOS ARCHIVOS ANTERIORES ===========
 
 echo "Restaurando Archivos"
+sudo rm -R -d /var/www/html
 sudo mv /etc/apache2/sites-available/000-default.conf.bak /etc/apache2/sites-available/000-default.conf
 sudo mkdir /var/www/html
